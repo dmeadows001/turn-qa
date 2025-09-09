66469ef169a6b547e89d37fa4f71a5836c77b09c
dmeadows001 <dexterlmeadows@gmail.com>
2025-09-08 20:46:37 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,8 @@
-
+export default function Home() {
+  return (
+    <div style={{ fontFamily: "sans-serif", textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello from Turn QA 👋</h1>
+      <p>If you see this page on your domain, your setup worked!</p>
+    </div>
+  )
+}
