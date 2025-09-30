ecddafd566d1b59070464775aa0047f60373f6ba
dmeadows001 <dexterlmeadows@gmail.com>
2025-09-29 20:02:47 -0700

    Update supabaseBrowser.ts

--- lib/supabaseBrowser.ts
+++ lib/supabaseBrowser.ts
@@ -2,7 +2,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const client =
-  // Reuse a single instance in the browser to avoid multiple GoTrue clients
   (typeof window !== 'undefined' && (window as any).__supabase__) ||
   createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
