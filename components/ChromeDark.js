fce2b4cefce9ab2b6d77014ed9b2f5bb09dfbb17
dmeadows001 <dexterlmeadows@gmail.com>
2025-10-04 20:59:52 -0700

    Update ChromeDark.js

--- components/ChromeDark.js
+++ components/ChromeDark.js
@@ -1,12 +1,22 @@
 // components/ChromeDark.js
 import { ui } from '../lib/theme';
+import UserMenu from './UserMenu';
 
 export default function ChromeDark({ title = 'TurnQA', children, max = 1040 }) {
   return (
     <div style={ui.page}>
       <main style={ui.wrap(max)}>
-        <header style={ui.header}>
+        <header
+          style={{
+            ...ui.header,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            gap: 12,
+          }}
+        >
           <div style={ui.title}>{title}</div>
+          <UserMenu />
         </header>
         {children}
       </main>
