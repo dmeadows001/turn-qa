0aff9001027c98e8b86e6e12135cbd0586ba7f6c
Dexter Meadows <dexterlmeadows@gmail.com>
2025-10-15 16:17:57 -0700

    chore: remove duplicate middleware.js

--- middleware.js
+++
@@ -1,9 +0,0 @@
-// middleware.js
-import { NextResponse } from 'next/server';
-
-// Only protect /admin now. Managers area is open while testing.
-export const config = { matcher: ['/admin/:path*'] };
-
-export default function middleware(_req) {
-  return NextResponse.next();
-}
