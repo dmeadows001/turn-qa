e5f522554c7776c875af7a2be23470712f7c7e91
dmeadows001 <dexterlmeadows@gmail.com>
2025-10-15 16:01:00 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,21 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  productionBrowserSourceMaps: true, // <-- enable readable stacks in prod
-};
-
-// next.config.js
-module.exports = {
   async redirects() {
     return [
-      {
-        source: '/turn/:id/capture',
-        destination: '/turns/:id/capture',
-        permanent: true, // or false while testing
-      },
+      { source: '/signin',      destination: '/login', permanent: true },
+      { source: '/auth/signin', destination: '/login', permanent: true },
     ];
   },
 };
 
 module.exports = nextConfig;
-
